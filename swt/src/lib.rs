9daa0b9af5c48b639cbcf05b9384d1c81404ea13
Andrey Suvorov <a.suvorov@sweatco.in>
2022-02-16 19:22:19 +0300

    oracle fee 🔮

--- swt/src/lib.rs
+++ swt/src/lib.rs
@@ -41,15 +41,22 @@ impl Contract {
 
     pub fn record_batch(&mut self, steps_batch: Vec<(AccountId, u32)>) {
         assert!(self.oracles.contains(&env::predecessor_account_id()));
+        let mut oracle_fee = 0f64;
         for (account_id, steps) in steps_batch.into_iter() {
             if !self.token.accounts.contains_key(&account_id) {
                 self.token.internal_register_account(&account_id);
             }
             let capped_steps = self.get_capped_steps(&account_id, steps);
-            let swt = self.formula(self.steps_from_tge, capped_steps);
-            self.token.internal_deposit(&account_id, swt.0 as u128);
+            let sweat_to_mint = self.formula(self.steps_from_tge, capped_steps);
+            let trx_oracle_fee = sweat_to_mint.0 as f64 * 0.05;
+            let minted_to_user = sweat_to_mint.0 as f64 - trx_oracle_fee;
+            oracle_fee = oracle_fee + trx_oracle_fee;
+            self.token
+                .internal_deposit(&account_id, minted_to_user as u128);
             self.steps_from_tge.0 += capped_steps as u64;
         }
+        self.token
+            .internal_deposit(&env::predecessor_account_id(), oracle_fee as u128);
     }
 
     pub fn formula(&self, steps_from_tge: U64, steps: u32) -> U128 {
