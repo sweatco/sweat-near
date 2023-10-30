88b3253d78218140f36d177b2d1bf69fce6a4adc
Vladas <146100@gmail.com>
2023-10-30 14:34:49 +0200

    added sweat integration crate

--- sweat/build.sh
+++ sweat/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo build -p sweat --target wasm32-unknown-unknown --release
 
 mkdir -p res
 cp target/wasm32-unknown-unknown/release/sweat.wasm res/sweat.wasm
